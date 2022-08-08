e29f582fdc805d226f09a0894716f8b8c705e2f5
DanielJackLouis <46109508+DanielJackLouis@users.noreply.github.com>
2022-08-08 10:20:41 +0100

    Initial Commit

---
+++ degenerateStringSearching/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  degenerateStringSearching
+//
+//  Created by 朱文尧 on 08/08/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
